b15f4b80116a5c88dd71b160ab3129d23a899507
Edgar Sanchez <edysanchez@gmail.com>
2014-01-23 03:18:36 +0100

    Add onmouseover handler on menu items Add some animation and style changes

--- js/respuesta.js
+++ js/respuesta.js
@@ -2,7 +2,7 @@ function createMenuItem(parentSelector,menuData){
     var menuListItem=document.createElement('li');
     $(menuListItem).addClass(menuData.class).appendTo(parentSelector);
     var menuLink=document.createElement('a');
-    $(menuLink).attr({"href":menuData.link}).html(menuData.name);
+    $(menuLink).attr({'href':menuData.link}).html(menuData.name);
     $(menuLink).appendTo(menuListItem);
 }
 function generateJSONMenu(url,menuListSelector) {
@@ -11,11 +11,28 @@ function generateJSONMenu(url,menuListSelector) {
             createMenuItem(menuListSelector,data[dataI]);
     }});
 }
+function addElementShadow(selector){
+    $(selector).css({'box-shadow':'-4px 8px 8px'});
+}
 function initialAnimation() {
-    $("div>div").fadeTo(1,30);
-    $(".left , .right").animate({"margin-left":0},5000);
+    $('div>div').fadeTo(1,1130);
+    $('.rojo,.azul,.verde').animate({'border-radius':30},3500);
+    addElementShadow('.rojo,.azul,.verde');
+    $('.left , .right').animate({'margin-left':0},3000);
+}
+function initMenuEvents(menuSelector){
+    $(menuSelector).on('mouseover','li',function() {
+        var destinationSelector='.left>div,.right>div';
+        var randomDestination=Math.ceil(Math.random()*10);
+        var destinationDivsCount=$(destinationSelector).length;
+        var text=document.createElement('h1');
+        $(text).html($(this).text());
+        $($(destinationSelector)[randomDestination%destinationDivsCount]).html('');
+        $($(destinationSelector)[randomDestination%destinationDivsCount]).append(text);
+    });
 }
 function init() {
-    generateJSONMenu('js/menu.json',".main-list");
+    generateJSONMenu('js/menu.json','.main-list');
     initialAnimation();
+    initMenuEvents('.main-list');
 }
